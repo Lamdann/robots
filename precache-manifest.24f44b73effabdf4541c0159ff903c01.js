c84bde83151d22d9729a678b5c00029d21c02596
Sergey_Krupnov <pishukub@gmail.com>
2019-12-06 00:55:01 +0200

    Updates

---
+++ precache-manifest.24f44b73effabdf4541c0159ff903c01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09237227549295d92d3db2800883b2b1",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "cd5ffea1fedf8aa3ba89",
+    "url": "/robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e3a91571bc0bbed0cc7a",
+    "url": "/robots/static/css/main.e78a93e2.chunk.css"
+  },
+  {
+    "revision": "cd5ffea1fedf8aa3ba89",
+    "url": "/robots/static/js/2.543709d7.chunk.js"
+  },
+  {
+    "revision": "e3a91571bc0bbed0cc7a",
+    "url": "/robots/static/js/main.10ed7677.chunk.js"
+  },
+  {
+    "revision": "a4a18463e94e0947d900",
+    "url": "/robots/static/js/runtime-main.41b51e89.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
